e6b39ade3662f02972feee93eab3e27dd0585afd
Matt Leinhos <matt@starlab.io>
2018-07-09 21:07:16 -0500

    Updated build script for spice

--- build-deb.sh
+++ build-deb.sh
@@ -3,9 +3,17 @@
 export FETCHER=/bin/false
 export WGET=/bin/false
 
-./configure --disable-docs --disable-stubdom \
-	--disable-qemu-traditional \
-	--with-system-qemu \
-	--with-system-seabios=/usr/share/seabios/bios.bin
+#./configure --disable-docs --disable-stubdom \
+#	--disable-qemu-traditional \
+#	--with-system-qemu \
+#	--with-platform=efi \
+#	--with-system-seabios=/usr/share/seabios/bios.bin
 
-make debball
+# If spice is enabled, then the build environment (docker?) needs spice packages.
+
+./configure --with-platform=efi \
+            --disable-stubdom   \
+            --with-extra-qemuu-configure-args="--enable-spice --enable-usb-redir"
+             
+
+make -j 12 debball
