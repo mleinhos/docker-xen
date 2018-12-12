5f73bf0adb0541ba1f716e37fa71e99614cc94cc
Matt Leinhos <matt.leinhos@twosixlabs.com>
2018-12-12 15:01:59 -0600

    Improved thread count.

--- build-deb.sh
+++ build-deb.sh
@@ -3,9 +3,12 @@
 export FETCHER=/bin/false
 export WGET=/bin/false
 
+export CONCURRENCY_LEVEL=`grep model_name /proc/cpuinfo | wc -l`
+
 ./configure --disable-docs --disable-stubdom \
 	--disable-qemu-traditional \
 	--with-system-qemu \
 	--with-system-seabios=/usr/share/seabios/bios.bin
 
-make debball
+
+make -j $CONCURRENCY_LEVEL debball
