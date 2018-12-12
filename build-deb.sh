7b03ce84fd16b047f975d523e3f8421605673418
Matt Leinhos <matt.leinhos@twosixlabs.com>
2018-12-12 15:01:59 -0600

    Improved thread count.

--- build-deb.sh
+++ build-deb.sh
@@ -3,17 +3,10 @@
 export FETCHER=/bin/false
 export WGET=/bin/false
 
-#./configure --disable-docs --disable-stubdom \
-#	--disable-qemu-traditional \
-#	--with-system-qemu \
-#	--with-platform=efi \
-#	--with-system-seabios=/usr/share/seabios/bios.bin
-
-# If spice is enabled, then the build environment (docker?) needs spice packages.
+export CONCURRENCY_LEVEL=`grep model_name /proc/cpuinfo | wc -l`
 
 ./configure --with-platform=efi \
             --disable-stubdom   \
             --with-extra-qemuu-configure-args="--enable-spice --enable-usb-redir"
              
-
-make -j 12 debball
+make -j $CONCURRENCY_LEVEL debball
